3305df6d125d822893bb3328590aa64287d31c5a
Student User <student@workstation.lab.example.com>
2024-06-22 03:09:05 -0400

    test

---
+++ chapter1-ge1.cmd
@@ -0,0 +1,20 @@
+oc login -u admin -p redhatocp https://api.ocp4.example.com:6443
+oc projects
+oc get nodes -o wide
+vim namespace.yml
+oc apply -f namespace.yml
+oc projects | grep openshift-cnv
+oc get all -n openshift-cnv
+oc get operatorgroups -A
+vim operatorgroup.yml
+oc apply -f operatorgroup.yml
+oc get operatorgroup -A
+oc get subscription -A
+vim  subscription.yml
+oc apply -f subscription.yml
+oc get subscription -A
+oc get csv -A
+vim hcooperator.yml
+oc apply -f hcooperator.yml
+
+
