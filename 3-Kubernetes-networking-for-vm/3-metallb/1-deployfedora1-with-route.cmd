fbab0ea1b6a2a4e9bf5a580f1ccd55c7255076a3
Student User <student@workstation.lab.example.com>
2024-06-27 08:29:46 -0400

    test1

--- 3-Kubernetes-networking-for-vm/3-metallb/1-deployfedora1-with-route.cmd
+++ 3-Kubernetes-networking-for-vm/3-metallb/1-deployfedora1-with-route.cmd
@@ -1,32 +1,65 @@
 oc login -u admin -p redhatocp https://api.ocp4.example.com:6443
-oc whoami --show-console
-oc new-project test100
-vim fedora1.yml
-oc apply -f fedora1.yml
 
-Apply app: myvm1 labels to vm fedora1 VM in section spec.template.metadata.labels and delete  VM instance [VMI]
+oc whoami --show-console
+
+oc new-project test100
+
+
+oc projects |grep metal
+
+oc get installplan,csv,subscription -n metallb-system
+
+
+oc get all -n metallb-system
+
+oc get pod -o wide -n metallb-system
+
+oc get daemonsets -n metallb-system
+
+oc get crd | grep metallb
+
+oc describe crd bfdprofiles
+
+oc get crd | grep metallb
+
+oc get metallb -n metallb-system
+
+oc get ipaddresspools -n metallb-system
+
+oc get l2advertisement -n metallb-system
+
+vim fedora1-ssh-web.yml
+oc apply -f fedora1-ssh-web.yml
+
+oc get vm/fedora1 -o yaml | grep -C 5 'kubevirt.io/domain'
+
+Add to VM in spec.template.metadata.labels:  kubevirt.io/domain: fedora1
 
 oc edit vm fedora1
 
-
 oc delete vmi fedora1
 
-oc create service clusterip fedora1 --tcp 80:80
-
-Apply app: myvm1 labels to service fedora1  in section spec.selector
-
-oc edit svc fedora1
-
-oc get endpoints
+oc get vm/fedora1 -o yaml | grep -C 5 'kubevirt.io/domain'
+oc get vm
 
 oc get vmi
 
-oc expose svc fedora1
+oc get pod
 
-oc get route
+POD=$(oc get pod -l kubevirt.io/domain=fedora1 -o name)
 
-curl http://
+echo $POD
 
-oc delete vm fedora1
-oc delete project test100
-END
+oc expose ${POD} --name=fedora1 --type=LoadBalancer --selector kubevirt.io/domain=fedora1 --port=22 -o yaml --dry-run=client | tee service.yaml
+
+cat service.yaml
+
+oc apply -f service.yaml
+
+oc get service
+
+oc get endpoints
+
+ssh fedora1@IP
+
+
