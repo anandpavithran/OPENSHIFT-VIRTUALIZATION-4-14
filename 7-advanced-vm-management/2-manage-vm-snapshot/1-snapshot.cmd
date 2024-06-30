999dd454cea3ee1810bbaeb446c7e058c5d8a826
Student User <student@workstation.lab.example.com>
2024-06-30 07:19:46 -0400

    test

--- 7-advanced-vm-management/2-manage-vm-snapshot/1-snapshot.cmd
+++ 7-advanced-vm-management/2-manage-vm-snapshot/1-snapshot.cmd
@@ -4,8 +4,8 @@ oc whoami --show-console
 
 oc new-project test100
 
-vim fedora1.yml
-oc create -f fedora1.yml 
+vim rhel2-dv-direct.yml
+oc create -f rhel2-dv-direct.yml 
 
 oc get dv
 
@@ -13,6 +13,35 @@ oc get pvc
 
 oc get pv
 
-./virtctl console fedora1 
+
+oc apply -f snapshot.yml
+
+oc get virtualmachinesnapshot
+
+oc get virtualmachinesnapshot
+
+oc get virtualmachinesnapshotcontents
+
+oc get virtualmachinesnapshotcontents
+
+
+oc get volumesnapshotclass
+
+./virtctl console rhel2 #add new file
+
+oc create -f restoresnap.yml
+./virtctl stop rhel2
+
+oc get vm
+
+oc create -f restoresnap.yml
+
+oc get virtualmachinerestore
+
+oc get virtualmachinerestore
+oc describe virtualmachinerestore
+
+
+./virtctl console rhel2 #check
 
 oc delete project test100
